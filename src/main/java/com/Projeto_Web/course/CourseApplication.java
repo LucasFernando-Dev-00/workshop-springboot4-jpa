f1d0efc0a65e4a3699a59e61eac3170bdffc7d0d
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-22 17:27:42 -0300

    Primeiro Commit

---
+++ src/main/java/com/Projeto_Web/course/CourseApplication.java
@@ -0,0 +1,17 @@
+package com.Projeto_Web.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+
+
+
+
+
+	}
+}
