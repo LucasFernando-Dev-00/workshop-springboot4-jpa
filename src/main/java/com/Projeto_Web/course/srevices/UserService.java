6d84f6e7e7a4254a3c5e518050bef37f9bda738f
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-23 17:41:33 -0300

    Adicionei o findAll e findById

---
+++ src/main/java/com/Projeto_Web/course/srevices/UserService.java
@@ -0,0 +1,26 @@
+package com.Projeto_Web.course.srevices;
+
+import com.Projeto_Web.course.entities.User;
+import com.Projeto_Web.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
