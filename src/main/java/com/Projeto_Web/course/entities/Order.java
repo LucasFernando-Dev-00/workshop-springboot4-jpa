788fe0891c8842b00ad8bd593eb20da1dcf94bba
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-24 17:48:00 -0300

    Adicionei a classe OrderEnum

--- src/main/java/com/Projeto_Web/course/entities/Order.java
+++ src/main/java/com/Projeto_Web/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.Projeto_Web.course.entities;
 
+import com.Projeto_Web.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -22,6 +23,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderstatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -30,9 +33,10 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +54,17 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderstatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderstatus) {
+        if (orderstatus != null) {
+            this.orderstatus = orderstatus.getCode();
+
+        }
     }
 
     public User getClient() {
