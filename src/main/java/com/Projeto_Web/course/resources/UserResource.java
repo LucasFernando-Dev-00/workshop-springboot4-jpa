07223db8f1eb0acc703ae0d2370c30af96bc2144
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-27 18:11:54 -0300

    Adicionei o Insert Usuário

--- src/main/java/com/Projeto_Web/course/resources/UserResource.java
+++ src/main/java/com/Projeto_Web/course/resources/UserResource.java
@@ -3,13 +3,13 @@ package com.Projeto_Web.course.resources;
 
 import com.Projeto_Web.course.entities.User;
 import com.Projeto_Web.course.srevices.UserService;
+import jakarta.persistence.SqlResultSetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,8 +29,14 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
-
-
-
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+         obj = service.insert(obj);
+         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+
 }
